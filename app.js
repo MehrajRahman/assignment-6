eab09b9d21b66ec7273a4e6600f5e65422b95fdc
MehrajRahman <rahmanmehraj627@gmail.com>
2021-02-14 12:44:44 +0600

    done

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let input = document.getElementById("search");
 // Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
+  if (event.key === "Enter") {
     // Cancel the default action, if needed
     event.preventDefault();
     // Trigger the button element with a click
